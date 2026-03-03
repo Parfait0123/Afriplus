51ccf1233bc245768057bb125bf551422857d15d
Parfait0123 <parfaitbot8@gmail.com>
2026-03-03 20:37:37 +0100

    Corrections

--- app/auth/connexion/page.tsx
+++ app/auth/connexion/page.tsx
@@ -4,6 +4,8 @@ import Link from "next/link";
 import { useRouter, useSearchParams } from "next/navigation";
 import { createClient } from "@/lib/supabase/client";
 
+export const dynamic = "force-dynamic";
+
 export default function ConnexionPage() {
   const router = useRouter();
   const searchParams = useSearchParams();
